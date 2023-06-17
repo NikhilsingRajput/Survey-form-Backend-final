0127bcc0d6d8c983bfd09c908873d31a2a70919f
Nikhilsing Kiransing Rajput <124592829+NikhilsingRajput@users.noreply.github.com>
2023-06-17 05:50:24 -0700

    Update route.js

--- route/route.js
+++ route/route.js
@@ -66,8 +66,8 @@ router.post('/signin', async (req, res) => {
                     res.cookie('jwtoken', token, {
                     // path: '/',
                     expires: new Date(Date.now() + 10000000), //10 min
-                    httpOnly: true,
-                    domain: 'localhost:3000'
+                    // httpOnly: true
+                    
                     // sameSite: "lax"
                 })
                 return res.status(200).json({ message: "Sign in Success", user: userlogin, token })
