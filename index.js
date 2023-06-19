e48595fe6710380046091c29fb8140922b171c15
Your NameNikhilsingRajput <nikhilsingrajput2016@gmail.com>
2023-06-19 18:50:05 +0530

    mistake url

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ mongoose
     });
 
 app.use(cookieparser())
-app.use(cors({credentials:true , origin:'http://localhost:3000'}));
+app.use(cors({credentials:true , origin:'https://survey-forms-project.onrender.com'}));
 // app.use(cors());
 app.use(bodyparser.urlencoded({ extended: false }));
 app.use(bodyparser.json());
