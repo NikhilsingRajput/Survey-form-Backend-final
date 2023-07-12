688af3b6684a979aeaf8aaa17c9a95fdd6689702
Nikhilsing Kiransing Rajput <124592829+NikhilsingRajput@users.noreply.github.com>
2023-07-12 05:09:22 -0700

    Updated cors policy for development index.js

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ mongoose
     });
 
 app.use(cookieparser())
-app.use(cors({credentials:true , origin:'https://survey-forms-project.onrender.com'}));
+app.use(cors({credentials:true , origin:'*'}));
 // app.use(cors());
 app.use(bodyparser.urlencoded({ extended: false }));
 app.use(bodyparser.json());
